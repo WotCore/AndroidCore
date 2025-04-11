d155a36a2570582111dcc4542f04708765cd887f
yangsn <ysn_2020@163.com>
2025-04-11 14:46:23 +0800

    初始化仓库

---
+++ app/src/main/kotlin/wot/core/demo/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package wot.core.demo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import wot.core.demo.R
+
+/**
+ * @author yangsn
+ * @date 2025/4/11
+ * @des
+ */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
